847db88712939d16fed03f77284a521adf1767ba
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-02-16 20:10:01 -0300

    finalizado

--- main.py
+++ main.py
@@ -5,52 +5,52 @@
 import numpy as np
 from copy import deepcopy
 from utils import *
+from random import randint
 
 import gymnasium as gym
 from pettingzoo.classic import tictactoe_v3
 
 
+def make_moves(action_mask):
+    list_possible_moves = []
+    for position, value in enumerate(action_mask):
+        if(value):
+            list_possible_moves.append(position)
+    return list_possible_moves
+
+def apply_action(possible_actions, position):
+    aux_list = deepcopy(possible_actions)
+    aux_list[position] = 0
+    return aux_list
+
 class Node():
-    def __init__(self,id_player, depth, board , action_mask , move):
+    def __init__(self,id_player, board , action_mask , move):
         
         self.id = id_player                                                                         # Identifica se o nó é do player_1 ou player_2
-        self.father_move = move if move != None else  play_random_agent(id_player, action_mask)    # Caso primeiro movimento                           
+        self.id_move = move if move != None else randint(0,8)                                       # Posição jogada que resultou nessa configuração de tabuleiro                          
         
         self.board = board                                                                          # Configuração board
         self.action_mask = action_mask
-        self.possible_moves = self.make_moves(action_mask)
-        self.depth = depth                                                                          # Profundidade
+        self.possible_moves = make_moves(action_mask)                                               # Profundidade
         self.children = []
 
     def create_children(self):
         id = 'player_1' if self.id == 'player_2' else 'player_2'
-        depth = self.depth - 1
 
         for move in self.possible_moves:
-            mask = self.apply_action(self.action_mask, move)
-            new_board = create_new_board(self.board , move)
+            mask = apply_action(self.action_mask, move)
+            new_board = create_new_board(self.board , move, id)
 
-            child = Node(id, depth, new_board, mask, self.father_move)
+            child = Node(id, new_board, mask, move)
             self.children.append(child)
 
-    def make_moves(self , action_mask):
-        list_possible_moves = []
-        for position, value in enumerate(action_mask):
-            if(value):
-                list_possible_moves.append(position)
-        return list_possible_moves
-    
-    def apply_action(self, possible_actions, position):
-        aux_list = deepcopy(possible_actions)
-        aux_list[position] = 0
-        return aux_list
+            #print(f"ID DAS CRIANÇAS : {move}")
 
 def play_random_agent(agent, mask):
     x = env.action_space(agent).sample()
     while mask[x] != 1:
         x = env.action_space(agent).sample()
     return x
-
 
 env = tictactoe_v3.env(render_mode='human')
 env.reset()
@@ -62,29 +62,23 @@ action = None
 
 while not_finish:
     for agent in ['player_1','player_2']:
-        observation, reward, termination, truncation, info = env.last() 
-
-        print(f'------- AGENTE : {agent}-------')
-        print(observation['action_mask'])
+        observation, reward, termination, truncation, info = env.last()
         if termination or truncation:
-            not_finish = False
+            not_finish = False 
         else:
             if agent == 'player_1':
-                
                 # Variaveis do nó
-                init_depth = 2
+                depth = 1
                 board = change_observation_format(observation)
-                root = Node(agent, init_depth, board , observation['action_mask'], action)
+                root = Node(agent, board , observation['action_mask'], action)
                 
                 # Ação por mim-max
-                value = min_max_agent(root) 
-                print(f'------ {value} -----') 
+                value , child = min_max_agent(root, depth, agent)
+                action = child.id_move
 
-                action = play_random_agent(agent,observation['action_mask'])     
-            
             else:
-                
                 action = play_random_agent(agent,observation['action_mask']) 
+            
             print(f'play: ',action)
             env.step(action)
 
--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 from copy import deepcopy
-import numpy as np
 
-def create_new_board(board , move):
-        
-        new_board = deepcopy(board)
-        
-        if(move in [0,3,6]):
-            new_board[0][move // 3] = [1,0]
-        elif(move in [1,4,7]):
-            new_board[1][move // 3] = [1,0]
-        else:
-            new_board[2][move //3] = [1,0]
-        
-        return new_board
+def create_new_board(board, move, id):
+
+    new_board = deepcopy(board)
+    key = [1,0] if id == 'player_1' else [0,1]
+
+    if(move in [0,3,6]):
+        new_board[0][move // 3] = key
+    elif(move in [1,4,7]):
+        new_board[1][move // 3] = key
+    else:
+        new_board[2][move // 3] = key
+
+    return new_board
 
 def calculate_static_value(agent , board):
     '''
@@ -20,20 +20,20 @@ def calculate_static_value(agent , board):
     '''
 
     #             -------- PONTUAÇÕES --------
-    # 100 pontos:
+    # 10 pontos:
     #
     #   X | X | X   ou   X |    ou    X |   |
     #                    X |            | X |
     #                    X |            |   | X
     #
 
-    # 10 pontos : 
+    # 10 pontos :
     #
     #   X | - | X   ou  X |   ou    X |   |
     #                   - |           | - |
     #                   X |           |   | X
 
-    # 1 ponto : 
+    # 1 ponto :
     #
     #   X |   |     ou   X |   ou    X |   |
     #                    X |           | O |
@@ -41,144 +41,160 @@ def calculate_static_value(agent , board):
 
     # Busca o mínimo ou o máximo?
     if(agent == 'player_1'):
-        factor = 1
+        me = [1,0]
+        adversary = [0,1]
     else:
-        factor = -1
+        me = [0,1]
+        adversary = [1,0]
 
-    me = [1,0]
-    adversary = [0,1]
     points = 0
-     
+
+    # print(board)
     for board_line in board:
         event_me = board_line.count(me)
         event_adversary = board_line.count(adversary)
 
         if(event_adversary == 3):
-            points += factor*(-1)*100             
+            points += (-1)*100
         elif(event_adversary == 2):
             if(event_me == 0):
-                points += factor*(-1)*10           
+                points += (-1)*100
             else:
-                points += factor*(-1)             
+                points += (-1)
         elif(event_adversary == 1):
-            points += factor*(-1)                     
-        
-        elif(event_me == 3):
-            points += factor*100              
+            points += (-1)
+
+        if(event_me == 3):
+            points += 100
         elif(event_me == 2):
             if(event_adversary == 0):
-                points += factor*10     
+                points += 10
             else:
-                points += factor
-
+                points += 1
         elif(event_me == 1):
-            points += factor                  
+            points += 1
+    # print(points)
 
-    
+
     # Casos 'me' ou 'adversary' na coluna
-    column_format = [[board[0][0] , board[1][0] , board[2][0]] , 
+    column_format = [[board[0][0] , board[1][0] , board[2][0]] ,
                     [board[0][1] , board[1][1] , board[2][1]] ,
-                    [board[0][2] , board[1][2] , board[2][2]] ,]
+                    [board[0][2] , board[1][2] , board[2][2]]]
 
-    for board_column in board:
+    # print(column_format)
+    for board_column in column_format:
         event_me = board_column.count(me)
         event_adversary = board_column.count(adversary)
 
         if(event_adversary == 3):
-            points += factor*(-1)*100           
-        elif(event_adversary == 2):           
-            if(event_me == 0):                
-                points += factor*(-1)*10
+            points += (-1)*100
+        elif(event_adversary == 2):
+            if(event_me == 0):
+                points += (-1)*10
             else:
-                points += factor*(-1)
+                points += (-1)
         elif(event_adversary == 1):
-            points += factor*(-1)              
-                                              
-        elif(event_me == 3):                  
-            points += factor*100             
-        elif(event_me == 2):                  
-            if (event_adversary == 0):        
-                points += factor*10           
+            points += (-1)
+
+        if(event_me == 3):
+            points += 100
+        elif(event_me == 2):
+            if (event_adversary == 0):
+                points += 10
             else:
-                points += factor
-        
-        elif(event_me == 1):                  
-            points += factor                  
-                                                      
+                points += 1
+        elif(event_me == 1):
+            points += 1
+
     # Casos 'me' ou 'adversary' na diagonal
     diagonal_format = [ [board[0][0] , board[1][1] , board[2][2]] , [board[0][2] , board[1][1] , board[2][0]]]
 
+    # print(diagonal_format)
     for diagonal in diagonal_format:
         event_me = diagonal.count(me)
         event_adversary = diagonal.count(adversary)
 
         if(event_adversary == 3):
-            points += factor*(-1)*100         
-        elif(event_adversary == 2):           
-            if(event_me == 0):                
-                points += factor*(-1)*10          
+            points += (-1)*100
+        elif(event_adversary == 2):
+            if(event_me == 0):
+                points += (-1)*10
             else:
-                points += factor*(-1)                
+                points += (-1)
         elif(event_adversary == 1):
-            points += factor*(-1)             
-                                              
-        elif(event_me == 3):                  
-            points += factor*100             
-        elif(event_me == 2):                  
+            points += (-1)
+        if(event_me == 3):
+            points += 100
+        elif(event_me == 2):
             if(event_adversary == 0):
-                points += factor*10
-            else:                          
-                points += factor
-        elif(event_me == 1):                 
-            points += factor    
-    
-    print(f'\n >> PONTOS: {points}\n ')
+                points += 10
+            else:
+                points += 1
+        elif(event_me == 1):
+            points += 1
 
-    return points  
+    # print(points)
+
+    return points
 
 def change_observation_format(obs):
     new_format = []
-    for i in range(0,3):
+    for i in range(3):
         aux = []
-        for position in obs['observation'][i]:
-            aux.append(position.tolist())
-        
+        for column in obs['observation']:
+            aux.append(column[i].tolist())
+
         new_format.append(aux)
-    
+
     return new_format
-        
-def min_max_agent(root):
+
+def min_max_agent(root, depth, agent):
     '''
     Maximizar ou minimizar lucro
     '''
 
-    if(root.depth == 0):
-        return calculate_static_value(root.id, root.board)
+    if(depth == 0):
+        # print(root.board, calculate_static_value(root.id, root.board), depth)
+        return calculate_static_value(agent, root.board) , root
 
-    # Cria filhos do nó:
-    root.create_children()
+    if(agent == 'player_1'):
+        max_value = -float('inf')
+        max_child = root
 
-    if(root.id == 'player_1'):
-        max_value = np.NINF
-        print(f"acoes = {root.possible_moves}")
-        print(f"--- FILHOS  : {len(root.children)}---")
+        # Cria filhos do nó:
+        root.create_children()
+
         for child in root.children :
+            value , child = min_max_agent(child, depth-1, 'player_2')
+            # print(child.board, value, depth)
 
-            print("---- BOARD:")
-            print(child.board)
+            if(value > max_value):
+                max_value = value
+                max_child = child
 
-            value = min_max_agent(child)
-            max_value = max(max_value, value)
-        
-        return max_value
-    
+        # print(f'MOVE MAX: {child.id_move}')
+        # print(max_child.board, max_value, depth)
+        return max_value , max_child
+
     else:
-        min_value = np.Inf
-        print("--- FILHOS  ---")
-        for child in root.children :
-            print(child.board)
+        min_value = float('inf')
+        min_child = root
 
-            value = min_max_agent(child)
-            min_value = min(min_value, value)
-    
-        return min_value
+        # Cria filhos do nó:
+        root.create_children()
+
+        for child in root.children :
+            value , child = min_max_agent(child, depth-1, 'player_1')
+            # print(child.board, value, depth)
+
+            if(value < min_value):
+                min_value = value
+                min_child = child
+
+        # print(f'MOVE MIN: {child.id_move}')
+        # print(min_child.board, min_value, depth)
+        return min_value , min_child
+
+def my_agent(agent , mask):
+    x = int(input("Por favor, digite sua jogada: "))
+    return x
