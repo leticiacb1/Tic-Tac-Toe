ca95ab137e8a0d662fc1135a2052feeacc6ee0cb
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-02-23 18:42:18 -0300

    Sobe random machine vs smart machine N times.

--- display.py
+++ display.py
@@ -19,18 +19,23 @@ def init_game_screen():
     print(" \n    > Choose a game option :  \n")
     print("    1 - Smart Machine (X)  vs  Random Machine (O)\n")
     print("    2 - Smart Machine (X)  vs  Human (O)\n")
+    print("    3 - Smart Machine (X)  vs  Random Machine (O) - 50 Times \n")
+    print("    4 - Smart Machine (X)  vs  Smart Machine (O) - 50 Times \n")
 
 
-    option = int(input("    > [1 or 2] ? \t"))
-    os.system('clear')
-
+    option = int(input("    > [1 or 2 or 3 or 4] ? \t"))
     return option
 
 def start_game():
+    
+    os.system('clear')
+
     print("\n     ------------------------------------")
     print("     --------- STARTING THE GAME --------")
     print("     ------------------------------------\n") 
 
+
+def infos_board():
     print(" \n    > Board positionsn :  \n")
 
     print("             |   |   ")
@@ -42,7 +47,7 @@ def start_game():
     print("             |   |   ")
     print("           2 | 5 | 8 ")
     print("             |   |   ")
-   
+
 def player_move(name , position):
 
     print("\n     ------------------------------------")
@@ -59,3 +64,15 @@ def end_screen(name , situacao):
     print("     ------------------------------------\n") 
     
     print("\n                  THE END !             ")
+
+def show_scores(dic):
+    print("\n")
+    print("\n     ------------------------------------")
+    print("        SCORES SMART MACHINE (PLAYER 1)")
+    print("     ------------------------------------\n") 
+
+    print(f" \n    > Vitórias : {dic['wins']}")
+    print(f" \n    > Empates : {dic['tied']}")
+    print(f" \n    > Derrotas : {dic['lose']}")
+
+    print("\n")
--- main.py
+++ main.py
@@ -11,51 +11,107 @@ def main(env, option):
     not_finish = True
     action = None
 
-    while not_finish:
-        for agent in ['player_1','player_2']:
-            
-            observation, reward, termination, truncation, info = env.last()
-            if termination or truncation:
-                not_finish = False 
-            else:
-                if agent == 'player_1':
-                    
-                    # Variaveis do nó
-                    depth = 1
-                    board = change_observation_format(observation)
-                    root = Node(agent, board , observation['action_mask'], action)
-                    
-                    # Ação por mim-max
-                    value , child = min_max_agent(root, depth, agent)
-                    action = child.id_move
-
-                    # Display:
-                    player_move('SMART MACHINE' , action)
-
-                else:
-
-                    if(option == 1):
-                        action = play_random_agent(env,agent,observation['action_mask']) 
-                        player_move('RANDOM MACHINE', action)
-                    else:
-                        action = my_agent(agent,observation['action_mask']) 
-
-                env.step(action)
+    # Variáveis para loops de jogadas
+    n_rounds = 50
+    count_rounds = 0
+    dic = {'wins': 0 , 'tied': 0 , 'lose': 0}
     
-    if(env.rewards['player_1'] > env.rewards['player_2']):
-        end_screen('SMART MACHINE', 'WIN')
-    elif(env.rewards['player_1'] == env.rewards['player_2']):
-        end_screen('TIED', 'GAME')
-    else:
-        print(" A maquina não perde, nem vou escrever essa possibilidade :)")
+    # Conição de parada:
+    stop = False
+
+    while not stop:
+
+        # Loop principal
+        while not_finish:
+            for agent in ['player_1','player_2']: 
+                observation, reward, termination, truncation, info = env.last()
+                if termination or truncation:
+                    not_finish = False 
+                else:
+                    if agent == 'player_1':
+                        
+                        # Variaveis do nó
+                        depth = 1
+                        board = change_observation_format(observation)
+                        root = Node(agent, board , observation['action_mask'], action)
+                        
+                        # Ação por mim-max
+                        value , child = min_max_agent(root, depth, agent)
+                        action = child.id_move
+
+                        # Display:
+                        if(option != 3 and option != 4):
+                            player_move('SMART MACHINE' , action)
+
+                    else:
+
+                        if(option == 1):
+                            action = play_random_agent(env,agent,observation['action_mask']) 
+                            player_move('RANDOM MACHINE', action)
+                        elif(option == 2):
+                            action = my_agent(agent,observation['action_mask']) 
+                        else:
+                            # Loop de jogadas
+                            if(option == 3):# Random
+                                action = play_random_agent(env,agent,observation['action_mask'])
+
+                    env.step(action)
+        
+
+        # Jogando apenas 1 vez:
+        if (option == 1 or option == 2): 
+
+            if(env.rewards['player_1'] > env.rewards['player_2']):
+                end_screen('SMART MACHINE', 'WIN')
+            elif(env.rewards['player_1'] == env.rewards['player_2']):
+                end_screen('TIED', 'GAME')
+            else:
+                print(" A maquina não perde, nem vou escrever essa possibilidade :)")
+
+            stop= True
+            break
+
+        else:
+            # Jogando 20 vezes:
+            count_rounds+=1
+
+            # Somatorio de vitorias, empates e derrotas
+            if(env.rewards['player_1'] > env.rewards['player_2']):
+                dic['wins']+=1
+            elif(env.rewards['player_1'] == env.rewards['player_2']):
+                dic['tied']+=1
+            else:
+                dic['lose']+=1
+        
+            if(count_rounds < n_rounds):
+                not_finish = True
+                action = None
+
+                # Cria ambiente:
+                env = tictactoe_v3.env()
+                env.reset()
+
+            else:
+                stop= True
+                show_scores(dic)
+
+                break
 
 if __name__ == '__main__': 
 
-    # Cria ambiente:
-    env = tictactoe_v3.env(render_mode='human')
-    env.reset()
-
     # Função principal
     option  = init_game_screen()
-    start_game()
+    
+    # Cria ambiente:
+    if(option == 1  or option == 2):
+        
+        start_game()
+        infos_board()
+
+        env = tictactoe_v3.env(render_mode='human')
+        env.reset()
+    else:
+        env = tictactoe_v3.env()
+        env.reset()
+
     main(env , option)
